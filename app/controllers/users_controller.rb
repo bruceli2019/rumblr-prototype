0b12672730d90a8a4752c084f328ff248db8e9ca
Bruce Li <pengyuli@chicagobooth.edu>
2019-02-02 22:22:01 +0000

    intermediate
    got the main photo working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,17 @@
 class UsersController < ApplicationController
     
     def index
+        
+        if current_user.nil?
+            @all_photos = Photo.new
+            @all_photos.user_id = -1 #dummy user_id
+            #dummy image
+            @all_photos.image = "https://as1.ftcdn.net/jpg/01/40/46/18/500_F_140461899_dvRngd7xvZtqCUHLiIyRjgflq2EmwnVP.jpg"
+            @all_photos.save
+        else
+            @all_photos = Photo.find(current_user.id)
+        end
+        
         render("user_templates/index.html.erb")
     end
 
